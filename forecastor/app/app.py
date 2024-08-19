f9e20108fed56ccdb2b38a9537165372f03da13d
Rijk van der Meulen <rijk.vandermeulen@eyeon.nl>
2024-08-19 19:52:05 +0200

    black

--- forecastor/app/app.py
+++ forecastor/app/app.py
@@ -5,6 +5,7 @@ from fastapi.templating import Jinja2Templates
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
+
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
