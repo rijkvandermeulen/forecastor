685fc86a60f15ca381adbc8af9103d47c55302d3
Rijk van der Meulen <rijk.vandermeulen@eyeon.nl>
2024-08-31 16:20:40 +0200

    first work on details dashboard

--- forecastor/app/app.py
+++ forecastor/app/app.py
@@ -36,11 +36,11 @@ async def get_high_level_summary_results(
 
     # Calculate forecast accuracy of the various forecast versions
     mae_perc_stat_fcst = min(1, (df["absolute_error_stat_fcst"].sum() / df["sales"].sum()))
-    fa_stat_fcst = 1 - mae_perc_stat_fcst
+    fa_stat_fcst = (1 - mae_perc_stat_fcst) * 100
     mae_perc_fin_fcst = min(1, (df["absolute_error_fin_fcst"].sum() / df["sales"].sum()))
-    fa_fin_fcst = 1 - mae_perc_fin_fcst
+    fa_fin_fcst = (1 - mae_perc_fin_fcst) * 100
     mae_perc_bm_fcst = min(1, (df["absolute_error_bm_fcst"].sum() / df["sales"].sum()))
-    fa_bm_fcst = 1 - mae_perc_bm_fcst
+    fa_bm_fcst = (1 - mae_perc_bm_fcst) * 100
 
     # Deltas
     fva_fin_stat = fa_fin_fcst - fa_stat_fcst
@@ -54,4 +54,16 @@ async def get_high_level_summary_results(
         "fva_bm_stat": fva_bm_stat
     }
 
-    return templates.TemplateResponse("results_summary.html", {"request": request, "kpis": kpis})
+    return templates.TemplateResponse("results_summary.html", {"request": request, "kpis": kpis, "session_id": session_id})
+
+
+@app.get("/details_enrichment", response_class=HTMLResponse)
+async def get_details_enrichment(
+        request: Request,
+        session_id: str = Query(...),
+        db: Session = Depends(get_db)
+):
+    res = db.query(models.SalesAndForecastData).filter(models.SalesAndForecastData.session_id == session_id).all()
+    df = pd.DataFrame([record.__dict__ for record in res])
+
+    return templates.TemplateResponse("details_enrichment.html", {"request": request, "session_id": session_id})
